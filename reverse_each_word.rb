0b549c196f93afebbbf43214230d6d406f39fa80
Allen Shin <geeuho@gmail.com>
2019-10-27 05:42:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,11 @@ def reverse_each_word (sentence)
   split_sentence = sentence.split(" ")
   split_sentence.each do |word|
     i = 0 
+    j = word_array.length - 1
     word_array = word.split("")
       while word_array[i] < word_array.length - 1
-        word_array[i] = word_array[j]
-end #=>
+            temp = temparray[i]; 
+            temparray[left] = temparray[right]; 
+            temparray[right]=temp
+  end
 end  
