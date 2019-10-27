806407628d6af8e79637a3851ac64650401b7a0b
Allen Shin <geeuho@gmail.com>
2019-10-27 05:41:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word (sentence)
   split_sentence.each do |word|
     i = 0 
     word_array = word.split("")
-      while word_array[i] < word_array[j]
+      while word_array[i] < word_array.length - 1
         word_array[i] = word_array[j]
 end #=>
 end  
